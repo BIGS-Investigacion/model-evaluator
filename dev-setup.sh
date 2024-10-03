6356f33df350d3b3f283796048524c775905043f
Jorge Garcia <jorgarcia@us.es>
2024-10-03 14:52:04 +0200

    Setup inicial

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,5 +1,9 @@
-#!/bin/zsh
-poetry .venv use 3.11 # Create the virtual environment if it does not exist
+#!/bin/bash
+python3 -m venv .venv #Create a virtual environtment
+source .venv/bin/activate
+pip install poetry
+poetry shell
+poetry install
 source $(poetry env info --path)/bin/activate # Activate and enter the virtual environment
 poetry install --with=dev # Install dev dependencies
 pre-commit install --install-hooks --overwrite -t pre-push # Set up pre-commit hooks
