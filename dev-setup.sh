f60a1d765abca1cea8f65015d97c60ff94aabf25
Jorge Garcia <jorgarcia@us.es>
2024-10-10 12:12:03 +0200

    Update token managing

--- dev-setup.sh
+++ dev-setup.sh
@@ -9,3 +9,4 @@ poetry install --with=dev # Install dev dependencies
 pre-commit install --install-hooks --overwrite -t pre-push # Set up pre-commit hooks
 export TTI_EVAL_CACHE_PATH=$PWD/.cache
 export TTI_EVAL_OUTPUT_PATH=$PWD/output
+export HF_TOKEN=<Your_Hugging_Face_API_Token>
